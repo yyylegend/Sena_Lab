dbd66f036488d8b574da4972995e3938fc1bef7f
yyylegend <randyyang123@hotmail.com>
2026-03-26 00:49:59 -0500

    update site URL to www.yyylegend.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://www.yyylegend.com/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
